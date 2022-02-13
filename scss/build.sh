eac81db51e97f5310ad9d8361a090a6fecb633fc
iiKurt <38019652+iiKurt@users.noreply.github.com>
2022-02-14 10:32:38 +1300

    Places to put your site-specific CSS rules

--- scss/build.sh
+++ scss/build.sh
@@ -1,7 +1,9 @@
 # The default theme
-sass themes/default/default-system-light-fallback.scss:build/theme.min.css --style compressed
+sass themes/default/default-system.scss:build/theme.min.css --style compressed
 sass themes/default/default-light.scss:build/theme-light.min.css --style compressed
 sass themes/default/default-dark.scss:build/theme-dark.min.css --style compressed
+
+# Compile site.scss and pages/*.scss here...
 
 # Standard/original, non-dark, non-themed Bootstrap fallback for printing and whatnot
 #sass original.scss:build/bootstrap.min.css --style compressed
