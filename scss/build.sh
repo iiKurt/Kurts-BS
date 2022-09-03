af1289dd1557b006dcc142613e6766a189e6ac06
iiKurt <38019652+iiKurt@users.noreply.github.com>
2022-09-04 10:02:23 +1200

    Support using site.scss
    Just uncomment the file in the build.sh script :)

--- scss/build.sh
+++ scss/build.sh
@@ -4,6 +4,7 @@ sass themes/default/light.scss:build/theme-light.min.css --style compressed
 sass themes/default/dark.scss:build/theme-dark.min.css --style compressed
 
 # Compile site.scss and pages/*.scss here...
+#sass site.scss:build/site.min.css --style compressed
 
 # Standard/original, non-dark, non-themed Bootstrap fallback for printing and whatnot
 #sass original.scss:build/bootstrap.min.css --style compressed
