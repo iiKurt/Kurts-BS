7301b578337e35512fd59c3d29955297ce7079c0
iiKurt <38019652+iiKurt@users.noreply.github.com>
2023-01-16 17:34:52 +1300

    Move to BS-5.3

--- build.sh
+++ build.sh
@@ -1,11 +1,2 @@
 # The default theme
-sass scss/themes/default/system-light-fallback.scss:build/theme.min.css --style compressed
-sass scss/themes/default/light.scss:build/theme-light.min.css --style compressed
-sass scss/themes/default/dark.scss:build/theme-dark.min.css --style compressed
-
-# Compile site.scss and pages/*.scss here...
-#sass site.scss:build/site.min.css --style compressed
-
-# Standard/original, non-dark, non-themed Bootstrap fallback for printing and whatnot
-#sass original.scss:build/bootstrap.min.css --style compressed
-#sass bootstrap-dark-5/bootstrap-original.scss:build/bootstrap.min.css --style compressed
+sass scss/default.scss:build/theme.min.css --style compressed
--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 # Updates/moves the built files into the web folder
 # Change this to suit your project :)
-cp build/*.min.css ../../../serve/css/
+cp build/*.min.css ../static/css/
